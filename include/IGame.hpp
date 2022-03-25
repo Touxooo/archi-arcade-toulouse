de1830f52868e44b24de0842aa12a197c119d0fa
Dommel Iona <iona.dommel-prioux@epitech.eu>
2022-03-25 15:48:03 +0100

    FIXED: types for DT

--- include/IGame.hpp
+++ include/IGame.hpp
@@ -8,9 +8,10 @@
 #ifndef IGAME_HPP_
 #define IGAME_HPP_
 
+#include <cstdint>
+
 #include "IEvent.hpp"
 #include "IScene.hpp"
-
 namespace arcade
 {
 
@@ -20,7 +21,7 @@ namespace arcade
         virtual ~IGame() = default;
 
         virtual IScene &init() = 0;
-        virtual IScene &update(const uint_64 &deltaTime) = 0;
+        virtual IScene &update(const std::uint64_t &deltaTime) = 0;
         // TODO: Replace raw pointer with reference / smart pointers
         virtual void manageEvents(IEvent *event) = 0;
         virtual void destroy() = 0;
--- include/IGraphical.hpp
+++ include/IGraphical.hpp
@@ -8,6 +8,7 @@
 #ifndef IGRAPHICAL_HPP_
 #define IGRAPHICAL_HPP_
 
+#include <cstdint>
 #include <functional>
 
 #include "IScene.hpp"
@@ -20,7 +21,7 @@ namespace arcade
         virtual ~IGraphical() = default;
 
         virtual void init(IScene &scene) = 0;
-        virtual void update(IScene &scene, const uint_64 &deltaTime) = 0;
+        virtual void update(IScene &scene, const std::uint64_t &deltaTime) = 0;
         virtual void destroy(IScene &scene) = 0;
 
     protected:
