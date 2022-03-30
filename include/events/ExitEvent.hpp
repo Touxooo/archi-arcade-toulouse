bc9a8a9b3b88d7cc800fa0aae4aeb47c29805789
Maux Camille <mcamille@localhost.localdomain>
2022-03-30 14:36:44 +0200

    [ADD] namespace arcade to events & ExitEvent

---
+++ include/events/ExitEvent.hpp
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2021
+** Untitled (Workspace)
+** File description:
+** ExitEvent.hpp
+*/
+
+#include "IEvent.hpp"
+
+namespace arcade {
+
+    class ExitEvent : public IEvent
+    {
+    };
+
+}
--- include/events/MouseEvent.hpp
+++ include/events/MouseEvent.hpp
@@ -9,21 +9,24 @@
 
 #include "IEvent.hpp"
 
-class MouseEvent : public IEvent
-{
-public:
-    enum Button {
-        None = -1,
-        Left,
-        Middle,
-        Right
+namespace arcade {
+
+    class MouseEvent : public IEvent
+    {
+    public:
+        enum Button {
+            None = -1,
+            Left,
+            Middle,
+            Right
+        };
+
+        MouseEvent(double x, double y, bool pressed = false, const Button& button = None)
+            : _btn(button), _x(x), _y(y), _isPressed(pressed) {};
+
+        Button _btn;
+        double _x;
+        double _y;
+        bool _isPressed;
     };
-
-    MouseEvent(double x, double y, bool pressed = false, const Button& button = None)
-        : _btn(button), _x(x), _y(y), _isPressed(pressed) {};
-
-    Button _btn;
-    double _x;
-    double _y;
-    bool _isPressed;
-};
+}
