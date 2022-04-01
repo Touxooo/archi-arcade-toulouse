f543529fda711415401e7c638d4bb77e2661da5c
JohannSako <johann.sako@epitech.eu>
2022-04-01 10:41:04 +0200

    [UPDATE] sceneSize method return double -> unsigned int

--- include/IScene.hpp
+++ include/IScene.hpp
@@ -22,8 +22,8 @@ namespace arcade
         virtual ~IScene() = default;
 
         virtual std::vector<std::unique_ptr<arcade::IEntity>> &getEntities() = 0;
-        virtual const double &getSceneWidth() = 0;
-        virtual const double &getSceneHeight() = 0;
+        virtual const uint32_t &getSceneWidth() = 0;
+        virtual const uint32_t &getSceneHeight() = 0;
 
     protected:
     private:
