c4e97804415e65a3e8347aeb1058a9c7ac522b8b
Maux Camille <mcamille@localhost.localdomain>
2022-04-02 18:14:00 +0200

    [MOD] MouseEvent bool _isPressed changed for ButtonState _state

--- include/events/MouseEvent.hpp
+++ include/events/MouseEvent.hpp
@@ -21,12 +21,12 @@ namespace arcade {
             Right
         };
 
-        MouseEvent(double x, double y, bool pressed = false, const Button &button = None)
-            : _btn(button), _x(x), _y(y), _isPressed(pressed){};
+        MouseEvent(double x, double y, ButtonState state = ButtonState::None, Button button = None)
+            : _btn(button), _x(x), _y(y), _state(state) {};
 
         Button _btn;
         double _x;
         double _y;
-        bool _isPressed;
+        ButtonState _state;
     };
 }
