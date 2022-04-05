b6692e93337f218d6cb57842afbe4375db8a3a88
JohannSako <johann.sako@epitech.eu>
2022-04-05 19:11:52 +0200

    [ADD] pause and resume method in IGame used to update the scene according to the game changing

--- include/IGame.hpp
+++ include/IGame.hpp
@@ -22,6 +22,8 @@ namespace arcade
 
         virtual IScene &init() = 0;
         virtual IScene &update(const std::uint64_t &deltaTime) = 0;
+        virtual IScene &pause();
+        virtual IScene &resume();
         virtual void manageEvents(IEvent &event) = 0;
         virtual void destroy() = 0;
 
