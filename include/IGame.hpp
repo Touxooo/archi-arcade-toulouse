2e610e0d7ac301b3e72aaa6d9e77a20b6b2716eb
marcoReguillot <marco.reguillot@epitech.eu>
2022-04-10 16:04:56 +0200

    [ADD] IScene and IGame documentation

--- include/IGame.hpp
+++ include/IGame.hpp
@@ -15,14 +15,30 @@
 namespace arcade
 {
 
+    /// This interface is used to describe a Game managed by arcade::ICore
     class IGame
     {
     public:
         virtual ~IGame() = default;
-
+        
+        /// This method is called one time when the game starts.
+        /// it inits the game assets properly
+        /// It returns a reference to `IScene` describing what the graphical part should display.
         virtual IScene &init() = 0;
+        
+        /// update` method is called in loop by ICore when the game is running.
+        /// it update the scenes in real time during the game
+        /// `deltaTime` parameter describes the time elapsed between the last call of `update`, it is used to update the game regardless of the FPS.
+        /// It returns a reference to `IScene` describing what the graphical part should display.
         virtual IScene &update(const std::uint64_t &deltaTime) = 0;
+
+        /// this method is called when an event was detected by IGraphical
+        /// `event` parameter describes the event, (refer to `IEvent`)
         virtual void manageEvents(IEvent &event) = 0;
+        
+        
+        /// `destroy` is called by ICore when the game needs to be stopped (when switching game, stopping arcade problem)
+        /// it allows to correctly reset the Game's elements
         virtual void destroy() = 0;
 
     protected:
--- include/IScene.hpp
+++ include/IScene.hpp
@@ -16,13 +16,22 @@
 namespace arcade
 {
 
+    /// this class is used to describe a scene,
+    /// a scene is a set of Entities usually created and modified by `IGame` and interpreted by `IGraphical`
     class IScene
     {
     public:
         virtual ~IScene() = default;
 
+        /// it returns a set of entities in the game
         virtual std::vector<std::shared_ptr<IEntity>> &getEntities() = 0;
+        
+        /// it returns the width of the scene,
+        /// 1 unity is equivalent to 1 character in a curse graphical system
         virtual uint32_t getSceneWidth() const = 0;
+ 
+        /// it returns the height of the scene,
+        /// 1 unity is equivalent to 1 character in a curse graphical system
         virtual uint32_t getSceneHeight() const = 0;
     protected:
     private:
