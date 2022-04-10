727ca25badc4920fb5e49525c52d14503e3586b6
Dommel Iona <iona.dommel-prioux@epitech.eu>
2022-04-10 15:49:06 +0200

    [REMOVED] an unapproved change in an enum

--- include/components/Sound.hpp
+++ include/components/Sound.hpp
@@ -8,50 +8,51 @@
 #ifndef SOUND_HPP_
 #define SOUND_HPP_
 
-#include "IComponent.hpp"
 #include <string>
+
+#include "IComponent.hpp"
 
 namespace arcade
 {
     /**
      * @brief The Sound component contain a filepath for the sound in .wav and a SoundStatus enum to set the status of the Sound being either PLAY, PAUSE OR STOP
      */
-    class Sound : public IComponent {
-        public:
-            /**
-             * @brief enum used to describe the status of the sound
-             * 
-             * @param PLAY if the sound should be played
-             * @param PAUSE if the sound should be paused
-             * @param STOP if the sound should be stopped
-             */
-            typedef enum SoundStatus_e {
-                PLAY,
-                PAUSE,
-                STOP,
-                LOOP
-            } SoundStatus_t;
+    class Sound : public IComponent
+    {
+    public:
+        /**
+         * @brief enum used to describe the status of the sound
+         *
+         * @param PLAY if the sound should be played
+         * @param PAUSE if the sound should be paused
+         * @param STOP if the sound should be stopped
+         */
+        typedef enum SoundStatus_e {
+            PLAY,
+            PAUSE,
+            STOP,
+        } SoundStatus_t;
 
-            /**
-             * @brief Construct a new Sound object
-             * 
-             * @param path the sound's file path (in .wav)
-             * @param status the sound's status
-             */
-            Sound(const std::string &path, SoundStatus_t status = SoundStatus_t::PLAY) : _status(status), _filepath(path) {};
-            ~Sound() = default;
+        /**
+         * @brief Construct a new Sound object
+         *
+         * @param path the sound's file path (in .wav)
+         * @param status the sound's status
+         */
+        Sound(const std::string &path, SoundStatus_t status = SoundStatus_t::PLAY) : _status(status), _filepath(path){};
+        ~Sound() = default;
 
-            /**
-             * @brief the sound's file path (in .wav)
-             */
-            SoundStatus_t _status;
-            /**
-             * @brief the sound's status
-             */
-            std::string _filepath;
+        /**
+         * @brief the sound's file path (in .wav)
+         */
+        SoundStatus_t _status;
+        /**
+         * @brief the sound's status
+         */
+        std::string _filepath;
 
-        protected:
-        private:
+    protected:
+    private:
     };
 }
 
